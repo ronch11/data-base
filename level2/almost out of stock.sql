35c835c9be4bbdbd0333d995d95d691b0248a141
yaron <yarongerstel@windowslive.com>
2021-04-14 14:29:00 +0300

    chaing name

--- level2/almost out of stock.sql
+++ level2/almost out of stock.sql
@@ -1,5 +1,4 @@
-select product.name,
-       description
+select product.name,description
 from product
          join Manufacturer M on product.manufacturld = M.id
          join (select * from Country where country_name = 'IL') C on M.countryid = C.id
