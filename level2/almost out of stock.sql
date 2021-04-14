6a76c8e16ae0c132d76be5c83e5935e929949280
Osher Zakin <osherz92@gmail.com>
2021-04-14 14:42:57 +0300

    Fix 'almost out of stock.sql' and using variables

--- level2/almost out of stock.sql
+++ level2/almost out of stock.sql
@@ -1,4 +1,9 @@
-select product.name,description
+# All products that almost out of stock from a specific country.
+set @country_name := 'GB';
+select product.*
 from product
-         join Manufacturer M on product.manufacturld = M.id
-         join (select * from Country where country_name = 'IL') C on M.countryid = C.id
+         join (select Manufacturer.id
+               from Manufacturer
+                        join Country C on Manufacturer.countryid = C.id
+               where country_name = @country_name) M on product.manufacturld = M.id
+where AmountInStock <= 2;
