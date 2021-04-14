c4961d210ba24156dc44d3da298aa74077f1e0a3
Osher Zakin <osherz92@gmail.com>
2021-04-14 14:56:47 +0300

    Add variable for min in stock

--- level2/almost out of stock.sql
+++ level2/almost out of stock.sql
@@ -1,9 +1,10 @@
 # All products that almost out of stock from a specific country.
 set @country_name := 'GB';
+set @min_amount := 2;
 select product.*
 from product
          join (select Manufacturer.id
                from Manufacturer
                         join Country C on Manufacturer.countryid = C.id
                where country_name = @country_name) M on product.manufacturld = M.id
-where AmountInStock <= 2;
+where AmountInStock <= @min_amount;
