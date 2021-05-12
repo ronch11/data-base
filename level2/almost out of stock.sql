b65b6050ee8e2b31475852d2ab6e7f684368c909
yaron <yarongerstel@windowslive.com>
2021-05-12 20:47:35 +0300

    yaron q

--- level2/almost out of stock.sql
+++ level2/almost out of stock.sql
@@ -1,12 +1,13 @@
 # All products that almost out of stock.
-select product.barcode,
+select product_in_branch.product_barcode,
        product.name,
+       product_in_branch.branch_id,
        M.name as manufaurer_name,
        C.country_name,
-       product.AmountInStock
-from product
+       product_in_branch
+from product join product_in_branch
 join  Manufacturer M on product.manufacturld = M.id
 join Country C on M.countryid = C.id
-where AmountInStock <= :min_amount;
+where product_in_branch.amount_in_stock <= :min_amount;
 
 
