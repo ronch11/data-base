01fee97369e5e91828950c5a2832b941efee191c
Osher Zakin <osherz92@gmail.com>
2021-06-03 17:08:44 +0300

    Update branchs table

--- level3/branchs.sql
+++ level3/branchs.sql
@@ -1,11 +1,9 @@
--- auto-generated definition
 create table branchs
 (
-    ID           int(10) auto_increment primary key,
-    TOWN         varchar(26) not null,
-    REVENUE      int(26)     not null,
-    MANAGER_NAME varchar(26) not null,
-    ADDRESS      varchar(50) not null,
-    AREA         int(10)     not null
+    ID      int(10) auto_increment
+        primary key,
+    TOWN    varchar(26) not null,
+    REVENUE int(26)     not null,
+    ADDRESS varchar(50) not null,
+    AREA    int(10)     not null
 );
-
