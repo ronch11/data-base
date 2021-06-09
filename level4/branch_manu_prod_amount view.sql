299c1d31e19c759f40ca2795eed82dd87c359f07
Osher Zakin <osherz92@gmail.com>
2021-06-09 21:39:50 +0300

    Edit view

--- level4/branch_manu_prod_amount view.sql
+++ level4/branch_manu_prod_amount view.sql
@@ -1,7 +1,10 @@
-create view branch_manu_prod_amount as (
-    select branch_id, m.id as  manu_id, sum(pib.amount_in_stock) as products_amount
-    from branchs
-             left join product_in_branch pib on branchs.ID = pib.branch_id
-             left join product p on p.barcode = pib.product_barcode
-             join Manufacturer m on m.id = p.manufacturld
-    group by branch_id, m.id);
+create or replace view branch_manu_prod_amount as
+(
+select concat(branchs.TOWN, ', ', branchs.ADDRESS) as branch_address,
+       m.name                              as manu_name,
+       sum(pib.amount_in_stock)            as products_amount
+from branchs
+         left join product_in_branch pib on branchs.ID = pib.branch_id
+         left join product p on p.barcode = pib.product_barcode
+         join Manufacturer m on m.id = p.manufacturld
+group by branch_id, m.id);
