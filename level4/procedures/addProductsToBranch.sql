f3a020b35a6edf2097e379672fdb13a6966edba5
Osher Zakin <osherz92@gmail.com>
2021-06-09 22:44:56 +0300

    Fix procedures

--- level4/procedures/addProductsToBranch.sql
+++ level4/procedures/addProductsToBranch.sql
@@ -1,5 +1,5 @@
-CREATE procedure addProductsToBranch(barcode varchar(25), amount float, price float, branchId int, datee DATE)
-# Add product amount that bought to the branch and add new outcome to manufacturer_expenses.
+create procedure addProductsToBranch(IN barcode varchar(25), IN amount float,
+                                                                 IN price float, IN branchId int, IN datee date)
 BEGIN
     # Insert new product amount, or if exist -> add amount.
     insert into product_in_branch(product_barcode, branch_id, amount_in_stock)
@@ -8,7 +8,6 @@ BEGIN
 
     # Insert new outcome.
     insert into manufacturer_expenses(manufacturer_id, date_of_expenses, expenses)
-    values ((select manufacturer_id from product p where p.barcode = barcode), datee, price);
+    values ((select manufacturld from product p where p.barcode = barcode), datee, price);
+    commit;
 END;
-
-DELIMITER ;
--- level4/procedures/buyProductFromBranch.sql
+++ level4/procedures/buyProductFromBranch.sql
@@ -7,10 +7,10 @@ BEGIN
     set current_amount := (select amount_in_stock
                            from product_in_branch pib
                            where pib.product_barcode = barcode
-                             and pib.branch_id = branch_id);
+                             and pib.branch_id = branchId);
 
     if current_amount < amount then
-        SIGNAL SQLSTATE '01000'
+        SIGNAL SQLSTATE '02000'
             set MESSAGE_TEXT = 'There are not enough amount';
     else
         # Reduce amount bought
@@ -19,13 +19,13 @@ BEGIN
         where pib.product_barcode = barcode
           and pib.branch_id = branchId;
 
-        set price := (select price from product p where p.barcode = barcode);
+        set price := (select p.ItemPrice from product p where p.barcode = barcode);
         # Increase income of branch
         update branchs b
         set b.REVENUE=b.REVENUE + price * amount
         where b.ID = branchId;
     end if;
-
+    commit;
 END;
 
 DELIMITER ;
